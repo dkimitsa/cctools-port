1fe24128385372467565042ea17216df5f52b5da
dkimitsa <demyan.kimitsa@gmail.com>
2017-11-16 13:35:45 +0200

    mingw: stubs to missing stdio blocking api

--- cctools/include/foreign/stdio.h
+++ cctools/include/foreign/stdio.h
@@ -4,6 +4,9 @@
  * work due to weird errors in libstdc++.
  */
 
+#ifndef __CCTOOLS_PORT_STDIO_H__
+#define __CCTOOLS_PORT_STDIO_H__
+
 #ifdef __CYGWIN__
 #pragma push_macro("__STRICT_ANSI__")
 #undef __STRICT_ANSI__
@@ -14,3 +17,16 @@
 #ifdef __CYGWIN__
 #pragma pop_macro("__STRICT_ANSI__")
 #endif /* __CYGWIN__ */
+
+
+#if (defined(_WIN32) || defined(__WIN32__)) && !defined(__CYGWIN__)
+#define flockfile(fh)
+#define funlockfile(fh)
+#define getc_unlocked(fh) getc(fh)
+#define setbuf(stream, buf)
+#define setbuffer(stream, buf, size)
+#define setlinebuf(stream)
+#define setvbuf(stream, buf, mode, size) 0
+#endif
+
+#endif /* __CCTOOLS_PORT_STDIO_H__ */
