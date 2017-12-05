a1aa7815bb95eb7afce99bb7c5abd63f0fb12a47
dkimitsa <demyan.kimitsa@gmail.com>
2017-12-05 09:56:21 +0200

    fixed: file binmode should be affecting only windows

--- cctools/libforeign/win32/binmode.c
+++ cctools/libforeign/win32/binmode.c
@@ -1,6 +1,9 @@
 //
 // dkimitsa: workaround for broken binmode.o in mingw.
 //
+#if (defined _WIN32 || defined __WIN32__) && ! defined __CYGWIN__
+
 #include <fcntl.h>
 int _fmode = _O_BINARY;
 
+#endif
